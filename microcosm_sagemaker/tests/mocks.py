b2630c916bda57ff3bee1d9734443eeaee18fcd8
Pokey Rule <pokey.rule@globality.com>
2019-05-15 18:22:34 +0100

    Fix

--- microcosm_sagemaker/tests/mocks.py
+++ microcosm_sagemaker/tests/mocks.py
@@ -7,32 +7,41 @@ from microcosm_sagemaker.tests.app_hooks.serve.app import create_app as create_s
 from microcosm_sagemaker.tests.app_hooks.train.app import create_app as create_train_app
 
 
-def create_app_hook_mock(name, load):
-    mock = MagicMock(name=name)
-    mock.configure_mock(
-        name=name,
-        load=load,
-    )
-    return mock
-
-
 @contextmanager
 def mock_app_hooks():
+    """
+    Mock all app hooks to those defined in microcosm_sagemaker.tests.app_hooks
+
+    """
     with patch(
         "microcosm_sagemaker.app_hooks.iter_entry_points",
         return_value=[
-            create_app_hook_mock(
+            _create_app_hook_mock(
                 name=TRAIN_APP_HOOK,
                 load=MagicMock(return_value=create_train_app),
             ),
-            create_app_hook_mock(
+            _create_app_hook_mock(
                 name=SERVE_APP_HOOK,
                 load=MagicMock(return_value=create_serve_app),
             ),
-            create_app_hook_mock(
+            _create_app_hook_mock(
                 name=EVALUATE_APP_HOOK,
                 load=MagicMock(return_value=create_evaluate_app),
             ),
         ]
     ):
         yield
+
+
+def _create_app_hook_mock(name, load):
+    """
+    We can't just instantiate a MagicMock with the right kwargs above because
+    the name parameter is expected by MagicMock itself.
+
+    """
+    mock = MagicMock(name=name)
+    mock.configure_mock(
+        name=name,
+        load=load,
+    )
+    return mock
