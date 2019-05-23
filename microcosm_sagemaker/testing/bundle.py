89cfae5c35db05f0f83a1b451d50efa3036ffb12
Pokey Rule <pokey.rule@globality.com>
2019-05-23 15:52:31 +0100

    Start to improve generic bundle test

--- microcosm_sagemaker/testing/bundle.py
+++ microcosm_sagemaker/testing/bundle.py
@@ -1,15 +1,15 @@
 import tempfile
+from abc import ABC, abstractmethod
 from pathlib import Path
 
-from hamcrest import assert_that, contains, has_properties
-
-from microcosm_sagemaker.app_hooks import create_train_app
+from microcosm_sagemaker.app_hooks import create_evaluate_app, create_train_app
 from microcosm_sagemaker.artifact import RootInputArtifact, RootOutputArtifact
+from microcosm_sagemaker.bundle import Bundle
 from microcosm_sagemaker.input_data import InputData
 from microcosm_sagemaker.testing.directory_comparison import directory_comparison
 
 
-class TestBundle:
+class TestBundle(ABC):
     def handle_setup(
         self,
         active_bundle: str,
@@ -23,38 +23,51 @@ class TestBundle:
             )
         )
         self.training_initializers = self.graph.training_initializers
+        self.active_bundle = active_bundle
 
         self.input_data = input_data
         self.input_artifact = input_artifact
         self.gold_output_artifact = gold_output_artifact
 
-    def check_bundle_prediction(self) -> None:
-        assert_that(
-            self.graph.simple_bundle.predict(1.0),
-            contains(has_properties(
-                uri="http://simple.com",
-                score=3.0,
-            )),
-        )
+    @abstractmethod
+    def check_bundle_prediction(self, bundle: Bundle) -> None:
+        ...
 
-    def test_fit(self) -> None:
+    def check_train(self) -> None:
         self.training_initializers.init()
-        self.graph.simple_bundle.fit(self.input_data)
-        self.check_bundle_prediction()
+        graph = create_train_app(
+            extra_config=dict(
+                active_bundle=self.active_bundle,
+            )
+        )
+        graph.train_active_bundle_and_dependencies(
+            gold_bundle_output_artifact,
+        )
+        self.check_bundle_prediction(self.active_bundle)
 
-    def test_load(self) -> None:
-        self.graph.simple_bundle.load(self.input_artifact)
-        self.check_bundle_prediction()
-
-    def test_save(self) -> None:
+    def check_save(self) -> None:
         self.graph.simple_bundle.load(self.input_artifact)
 
         with tempfile.TemporaryDirectory() as output_artifact_path:
-            output_artifact = OutputArtifact(output_artifact_path)
+            output_artifact = RootOutputArtifact(output_artifact_path)
 
-            self.graph.simple_bundle.save(output_artifact)
+            graph.train_active_bundle_and_dependencies(
+                gold_bundle_output_artifact,
+            )
 
             directory_comparison(
                 gold_dir=self.gold_output_artifact_path,
                 actual_dir=Path(output_artifact_path),
             )
+
+    def check_load(
+        self,
+        root_input_artifact: RootInputArtifact,
+    ) -> None:
+        graph = create_evaluate_app(
+            extra_config=dict(
+                active_bundle=self.active_bundle,
+            )
+        )
+        graph.load_active_bundle_and_dependencies(root_input_artifact)
+        self.check_bundle_prediction(self.active_bundle)
