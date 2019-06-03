4b69afbd8d3a7549b633c5078cc24c5affd136be
Pokey Rule <pokey.rule@globality.com>
2019-06-03 14:36:05 +0100

    Fix simple bundle prediction checkers

--- microcosm_sagemaker/tests/bundles/test_simple_bundle.py
+++ microcosm_sagemaker/tests/bundles/test_simple_bundle.py
@@ -11,27 +11,24 @@ from microcosm_sagemaker.tests.fixtures import get_fixture_path
 from microcosm_sagemaker.tests.mocks import mock_app_hooks
 
 
-bundle_prediction_checks = [
-    BundlePredictionCheck(
-        args=[1.0],
-        return_value_matcher=contains(
-            has_properties(
-                uri="http://simple.com",
-                score=3.0,
-            ),
-        )
-    )
-]
-
-
 class SimpleBundleTestCase(BundleTestCase):
     bundle_name = "simple_bundle"
     root_input_artifact_path = get_fixture_path("artifact")
+    bundle_prediction_checks = [
+        BundlePredictionCheck(
+            args=[1.0],
+            return_value_matcher=contains(
+                has_properties(
+                    uri="http://simple.com",
+                    score=3.0,
+                ),
+            )
+        )
+    ]
 
 
 class TestSimpleBundleFit(BundleFitTestCase, SimpleBundleTestCase):
     input_data_path = get_fixture_path("input_data")
-    bundle_prediction_checks = bundle_prediction_checks
 
     @mock_app_hooks()
     def setup(self) -> None:
@@ -49,8 +46,6 @@ class TestSimpleBundleSave(BundleSaveTestCase, SimpleBundleTestCase):
 
 
 class TestSimpleBundleLoad(BundleLoadTestCase, SimpleBundleTestCase):
-    bundle_prediction_checks = bundle_prediction_checks
-
     @mock_app_hooks()
     def setup(self) -> None:
         super().setup()
