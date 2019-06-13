b80fb55014b4fec8439ad7c094cca3aae61c3541
Salman Mashayekh <salman.mashayekh@globality.com>
2019-06-13 14:18:34 -0700

    * add typehinting to pickle comparison.

--- microcosm_sagemaker/testing/pickle_comparison.py
+++ microcosm_sagemaker/testing/pickle_comparison.py
@@ -1,6 +1,7 @@
-from functools import partial
 from io import BytesIO
 from pathlib import Path
+from typing import List, Union, Any, Mapping, BinaryIO, Optional
+from hamcrest.core.string_description import StringDescription
 
 import numpy as np
 from hamcrest.core.base_matcher import BaseMatcher
@@ -11,8 +12,7 @@ from microcosm_sagemaker.testing.bytes_extractor import ExtractorMatcherPair
 
 class IsObjectEqual(BaseMatcher):
     """
-    Matcher to compare the object blob saved at filepath A with another object blob saved
-    at filepath B.
+    Matcher to compare the object A with another object B.
     Sometimes there is some randomness within these generated files, so the user can optionally
     provide a `ignore_properties` array which will ignore mismatches of those given object
     properties.  For instance, if we have:
@@ -20,12 +20,13 @@ class IsObjectEqual(BaseMatcher):
     MyObject(foo=bar, extras=Test2)
     If ignore_properties is not specified, these will be unequal.  If ignore_properties is
     set to ["extras"], they will be equal.
+
     """
-    def __init__(self, obj, ignore_properties=None):
+    def __init__(self, obj: Any, ignore_properties: List = []) -> None:
         self.ignore_properties = ignore_properties
         self.compare_object = obj
 
-    def _matches(self, item):
+    def _matches(self, item: Any) -> bool:
         compare_a = self._get_dict_structure(self.compare_object)
         compare_b = self._get_dict_structure(item)
 
@@ -51,40 +52,44 @@ class IsObjectEqual(BaseMatcher):
 
         return True
 
-    def _get_dict_structure(self, obj):
+    def describe_to(self, description: StringDescription) -> None:
+        print(type(description))
+        description.append_text(str(self.compare_object))
+
+    def _get_dict_structure(self, obj: Any) -> Mapping[str, Any]:
         if isinstance(obj, dict):
             return obj
         return vars(obj)
 
 
-def pickle_extractor(binary):
+def pickle_extractor(binary: BinaryIO) -> Any:
     return joblib.load(BytesIO(binary))
 
 
 def matches_object(
     obj: Union[object, dict],
-    ignore_properties: Optional[List[str]]=None,
-):
+    ignore_properties: Optional[List[str]] = None,
+) -> IsObjectEqual:
     if ignore_properties is None:
         ignore_properties = []
     return IsObjectEqual(obj, ignore_properties)
 
 
-def matches_with_object(obj):
-    return partial(_matches_with_object, ignore_properties=[])
+def matches_with_object(obj: Any) -> IsObjectEqual:
+    return matches_object(obj)
 
 
-def _get_dir_pickles(dir, pickle_suffixes):
+def _get_dir_pickles(dir: Path, pickle_suffixes: List[str]) -> List[Path]:
     return [
         subpath.relative_to(dir)
-        for subpath in dir.glob('**/*')
+        for subpath in dir.glob("**/*")
         if subpath.suffix in pickle_suffixes
     ]
 
 
-def _create_matchers(dir_pickles):
+def _create_matchers(dir_pickles: List[Path]) -> Mapping[Path, ExtractorMatcherPair]:
     return {
-        Path(path): ExtractorMatcherPair(
+        path: ExtractorMatcherPair(
             pickle_extractor,
             matches_with_object,
         )
@@ -92,7 +97,7 @@ def _create_matchers(dir_pickles):
     }
 
 
-def create_pickle_matchers_for_dir(dir, pickle_suffixes=['.pk', '.pickle', '.pkl']):
+def create_pickle_matchers_for_dir(dir, pickle_suffixes=[".pk", ".pickle", ".pkl"]):
     dir_pickles = _get_dir_pickles(dir, pickle_suffixes)
     matchers = _create_matchers(dir_pickles)
     return matchers
