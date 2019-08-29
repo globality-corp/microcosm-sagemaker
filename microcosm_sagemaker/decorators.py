50c8982432a9cf60a50c9676a1d578c638822931
Salman Mashayekh <48693449+salmanmashayekh@users.noreply.github.com>
2019-08-29 12:50:18 -0700

    Add the `log_bundle_methods` decorator (#66)
    * add the decorator
    
    * add the decorator
    
    * reuse code better
    
    * linting
    
    * Update microcosm_sagemaker/decorators.py
    
    Co-Authored-By: Pokey Rule <pokey.rule@globality.com>
    
    * Update microcosm_sagemaker/decorators.py
    
    Co-Authored-By: Pokey Rule <pokey.rule@globality.com>
    
    * * import timer from microcosm-logging
    * do not add the `bundle_name` to bundles in the `log_bundle_methods` decorator
    
    * linting

--- microcosm_sagemaker/decorators.py
+++ microcosm_sagemaker/decorators.py
@@ -1,6 +1,9 @@
+import logging
 from typing import Any, Callable
 
+from microcosm.api import get_component_name
 from microcosm.object_graph import ObjectGraph
+from microcosm_logging.timing import elapsed_time
 
 
 def training_initializer():
@@ -17,3 +20,35 @@ def training_initializer():
             return component
         return factory
     return decorator
+
+
+def _method_with_logging(original_method):
+    def new_method(self, *args, **kwargs):
+        bundle_name = get_component_name(self._graph, self)
+        logging.info(
+            f"Started method `{original_method.__name__}` of the `{bundle_name}`."
+        )
+        timing = {}
+        with elapsed_time(timing):
+            original_method(self, *args, **kwargs)
+        logging.info(
+            f"Completed method `{original_method.__name__}` of the `{bundle_name}` "
+            f"after {timing['elapsed_time']/1000:.1f} seconds."
+        )
+    return new_method
+
+
+def log_bundle_methods(cls):
+
+    _init = cls.__init__
+
+    def __init__(self, graph: ObjectGraph, **kwargs) -> None:
+        _init(self, graph, **kwargs)
+        self._graph = graph
+
+    cls.__init__ = __init__
+    cls.fit = _method_with_logging(cls.fit)
+    cls.load = _method_with_logging(cls.load)
+    cls.save = _method_with_logging(cls.save)
+
+    return cls
